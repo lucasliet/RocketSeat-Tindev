d74598ff20d6e47821ed50d15c5e7d88714ffb8a
lucasliet <lucasouliveira@gmail.com>
2019-08-15 01:59:53 -0300

    update shellscript

--- install_nodejs_run_as_adm.ps1
+++ install_nodejs_run_as_adm.ps1
@@ -1,3 +1,5 @@
+(Set-ExecutionPolicy Unrestricted);
 (Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')));
 (cinst nodejs.install --lts);
-(choco install yarn)
+(choco install -y yarn python2 jdk8);
+(yarn global add react-native-cli)
