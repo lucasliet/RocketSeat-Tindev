4d8cfa56bff24489b74ecb48784f4d98b69e45a4
lucasliet <lucasouliveira@gmail.com>
2019-09-06 14:23:45 -0300

    delete install node

--- install_nodejs_run_as_adm.ps1
+++
@@ -1,5 +0,0 @@
-(Set-ExecutionPolicy Unrestricted);
-(Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')));
-(cinst nodejs.install --lts);
-(choco install -y yarn python2 jdk8);
-(yarn global add react-native-cli)
